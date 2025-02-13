a0ae43bbd5c77b262ab8a062f58ee57dbda17e65
reginearchico <reginearchico@gmail.com>
2025-02-13 07:33:34 -0500

    Update ServizioRest.js

--- RESTful/ServizioRest.js
+++ RESTful/ServizioRest.js
@@ -19,15 +19,17 @@ xmlhttp.onreadystatechange = function() {
         var file = JSON.parse(this.responseText);
         var table = document.getElementById("Visualizza");  
         table.innerHTML = "";
-        var tr = document.createElement("tr");
-        var tdDate = document.createElement("td");
-        tdDate.textContent = file.date;
-        tr.appendChild(tdDate);
-        var tdTitle = document.createElement("td");
-        tdTitle.textContent = file.title;
-        tr.appendChild(tdTitle);
-        table.appendChild(tr);
+        for (var x in file) {
+            var tr = document.createElement("tr");
+            var tdDate = document.createElement("td");
+            tdDate.textContent = file[x].date;
+            tr.appendChild(tdDate);
+            var tdTitle = document.createElement("td");
+            tdTitle.textContent = file[x].title;
+            tr.appendChild(tdTitle);
+            table.appendChild(tr);
+        }
     }
 };
-xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", true);
+xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5", true);
 xmlhttp.send();
