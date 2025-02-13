96a53e644efda321f497270eca3ba6f39f778360
reginearchico <reginearchico@gmail.com>
2025-02-13 07:27:00 -0500

    Update ServizioRest.js

--- RESTful/ServizioRest.js
+++ RESTful/ServizioRest.js
@@ -1,4 +1,4 @@
-var xmlhttp = new XMLHttpRequest();
+/*var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     var out = "";
     if (this.readyState == 4 && this.status == 200) {
@@ -12,4 +12,22 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "https://seahawkmedia.com/wp-json/wp/v2/posts?search=Drupal", true);
 xmlhttp.send();
 //https://www.desrparcosud.it/wp-json/wp/v2/posts?search=AMAP
-//https://seahawkmedia.com/wp-json/wp/v2/posts?search=Drupal
+//https://seahawkmedia.com/wp-json/wp/v2/posts?search=Drupal*/
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var file = JSON.parse(this.responseText);
+        var table = document.getElementById("Visualizza");  
+        table.innerHTML = "";
+        var tr = document.createElement("tr");
+        var tdDate = document.createElement("td");
+        tdDate.textContent = file.date;
+        tr.appendChild(tdDate);
+        var tdTitle = document.createElement("td");
+        tdTitle.textContent = file.title;
+        tr.appendChild(tdTitle);
+        table.appendChild(tr);
+    }
+};
+xmlhttp.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", true);
+xmlhttp.send();
